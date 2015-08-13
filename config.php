aa015ac5781cb4e0dbb098ba6837f0ce8a89736a
Igor Bronovskyi <admin@brun.if.ua>
2015-08-13 11:00:35 +0300

    Add config file

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+
+/* 
+ * Copyright (C) 2015 Brun
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+    $username = 'root';
+    $password = 'pass';
+
+    $conn = new PDO('mysql:host=localhost;dbname=nung_admiss-rating', $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
